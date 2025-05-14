38804bfc122ce82bd18b66d90ea8a32bb26bca9f
xinchen <283235702@qq.com>
2025-05-14 22:36:49 +0800

    fix: add comment

--- internal/parser/common.go
+++ internal/parser/common.go
@@ -8,6 +8,7 @@ import (
 	gen "github.com/xinchentechnote/fin-protoc/internal/grammar"
 )
 
+// ReadFileToString reads the content of a file and returns it as a string.
 func ReadFileToString(filename string) (string, error) {
 	data, err := os.ReadFile(filename)
 	if err != nil {
@@ -15,6 +16,8 @@ func ReadFileToString(filename string) (string, error) {
 	}
 	return string(data), nil
 }
+
+// NewPacketDslParserByFile creates a new PacketDslParser instance from a file.
 func NewPacketDslParserByFile(filename string) (*gen.PacketDslParser, error) {
 	data, err := ReadFileToString(filename)
 	if err != nil {
@@ -23,6 +26,7 @@ func NewPacketDslParserByFile(filename string) (*gen.PacketDslParser, error) {
 	return NewPacketDslParserByContent(data)
 }
 
+// NewPacketDslParserByContent creates a new PacketDslParser instance from a string.
 func NewPacketDslParserByContent(data string) (*gen.PacketDslParser, error) {
 	// 创建一个新的输入流
 	input := antlr.NewInputStream(string(data))
