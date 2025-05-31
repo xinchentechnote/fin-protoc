820808d40b2bcdca6c1e73ffeaf4dc5b4265f358
xinchen <xinchentechnote@163.com>
2025-05-31 11:14:44 +0800

    feat:build osxcross shared lib

--- lib/libpacketdsl.h
+++ lib/libpacketdsl.h
@@ -92,7 +92,7 @@ extern "C" {
 //
 //	Formatted DSL as C string, or error message if formatting fails
 //
-extern char* FormatPacketDslExport(char* dsl);
+extern __declspec(dllexport) char* FormatPacketDslExport(char* dsl);
 
 #ifdef __cplusplus
 }
