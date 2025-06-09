adccc67bab4c5d4eca809a7df8a22e536b6a959c
xinchen <283235702@qq.com>
2025-06-09 22:20:55 +0800

    fix:fix fmt

--- internal/parser/rust_generator_test.go
+++ internal/parser/rust_generator_test.go
@@ -482,7 +482,7 @@ func TestTestValue(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			generator := NewRustGenerator(config, binModel)
-			result := generator.TestValue(tt.parent, tt.field)
+			result := generator.testValue(tt.parent, tt.field)
 			assert.Equal(t, tt.expected, result)
 		})
 	}
