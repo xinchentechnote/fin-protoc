88482e9be4b9d9c9b36f89a5344c77558f154022
xinchen <xinchentechnote@163.com>
2025-07-31 19:41:37 +0800

    feat:support default subcmd to be compile

--- cmd/root.go
+++ cmd/root.go
@@ -8,14 +8,27 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "finprotoc",
+	Use:   "fin-protoc",
 	Short: "A CLI for compiling/formatting packet DSL",
 }
 
 // Execute runs the root command.
 func Execute() {
+	if len(os.Args) == 1 || (len(os.Args) > 1 && !isSubcommand(os.Args[1])) {
+		// insert "compile"
+		os.Args = append([]string{os.Args[0], "compile"}, os.Args[1:]...)
+	}
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func isSubcommand(arg string) bool {
+	for _, c := range rootCmd.Commands() {
+		if c.Name() == arg {
+			return true
+		}
+	}
+	return false
 }
